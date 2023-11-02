581cdaab731577edbe766c6474093d7431f518f2
SashaToledo <113081224+SashaToledo@users.noreply.github.com>
2023-11-02 22:49:42 +0000

    diseño adaptativo

--- app.js
+++ app.js
@@ -4,4 +4,3 @@ const path = require ('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
 app.listen(3030, () => console.log ("Abierto"));
-
